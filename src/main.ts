dbd4e7c9269b6ae4992968b769f5bb2c9e75f411
Carlos Aurelio Briceño Gomez <aurel.gomez@outlook.com>
2023-08-12 16:47:03 -0500

    initial

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.setGlobalPrefix('api');
+
+  app.use(helmet());
+
+  if (process.env.REDIS_URL) {
+    app.connectMicroservice({
+      transport: Transport.REDIS,
+      options: {
+        url: process.env.REDIS_URL,
+      },
+    });
+
+    await app.startAllMicroservices();
+  }
+
+  await app.listen(process.env.PORT);
+}
+bootstrap();
