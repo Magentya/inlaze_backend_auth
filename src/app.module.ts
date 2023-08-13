9a1537951849d34e7bb873ade1a42fe4fece097d
Carlos Aurelio Briceño Gomez <aurel.gomez@outlook.com>
2023-08-13 01:59:49 -0500

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseOptions } from './database/database.provider';
+import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseOptions)],
+  imports: [TypeOrmModule.forRoot(databaseOptions), UserModule, RoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
